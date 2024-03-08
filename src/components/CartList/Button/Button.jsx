cb8b3877f22f47fecaf8e724616e2d8177a3709b
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-03-08 19:30:21 +0200

    math

--- src/components/CartList/Button/Button.jsx
+++ src/components/CartList/Button/Button.jsx
@@ -1,8 +1,21 @@
+import { useDispatch } from "react-redux";
 import styled from "./Button.module.scss";
+import { addCount, minusCount } from "/src/store/cartSlice/cartSlice";
 
 const Button = ({ type: Type, role, count, id }) => {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    if (role === "increment") {
+      dispatch(addCount({ id, count }));
+    }
+    if (role === "decrement") {
+      dispatch(minusCount({ id, count }));
+    }
+  };
+
   return (
-    <button type="button" className={styled.buttons}>
+    <button type="button" className={styled.buttons} onClick={handleClick}>
       <Type />
     </button>
   );
--- src/store/cartSlice/cartSlice.js
+++ src/store/cartSlice/cartSlice.js
@@ -10,8 +10,22 @@ const cartSlice = createSlice({
       const newCart = { ...payload, count: 1 };
       state.cartList.push(newCart);
     },
+    addCount: (state, { payload }) => {
+      state.cartList = state.cartList.map((item) => {
+        return payload.id === item.id
+          ? { ...item, count: payload.count + 1 }
+          : item;
+      });
+    },
+    minusCount: (state, { payload }) => {
+      state.cartList = state.cartList.map((item) => {
+        return payload.id === item.id
+          ? { ...item, count: payload.count - 1 }
+          : item;
+      });
+    },
   },
 });
 
 export default cartSlice.reducer;
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, addCount, minusCount } = cartSlice.actions;
